81a9a81a608dc4e4d078d975f0cafa5a63b0b1b4
winner123421 <75743503+winner123421@users.noreply.github.com>
2020-12-09 15:46:03 +0000

    Test

---
+++ idkwhatthisis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cin >> name;
+    //string name = "world";
+    cout<<"Hello "<<name<<"\n";
+
+
+    return 0;
+}
