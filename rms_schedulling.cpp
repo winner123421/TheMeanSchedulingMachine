897801e72eb19c2106bb08f18a025937eb5e9dbf
Sulaiman <abdusulaiman1999@gmail.com>
2020-12-16 14:55:07 +0000

    RMS DONE

--- rms_schedulling.cpp
+++ rms_schedulling.cpp
@@ -64,85 +64,9 @@ vector <int> find_priority(vector <int> p){
         }    
     }
     return index;
-
-
-
-
-
-
    
-   
-   /*while (priority.size()<p.size()){
-       for(size_t n ; n<p.size();n++){
-           for(size_t m ; m<p.size();m++){
-               if (p[n]<p[m]){
-                   for(size_t b; b<priority.size(); b++){
-                       if (n == prior[b]){
-                           break;
-                       }
-                   }
-               }
-           }
-       }
-   }*/
-   
-   /* while(true){
-        
-
-        if (unsorted >0){
-            
-            for (size_t n=0; n<p.size();n++){
-                flag = true;
-               for (size_t m=0; m<p.size();m++){
-                   
-                   for(size_t c=0; c<priority.size();c++){
-                            if (n==priority[c]){
-                                flag=false;
-                            }
-
-                        }
-                    if (flag == false)break;
-                    else if(p[n]>p[m]){
-                        cout<<"here  \n";
-
-                        flag = false;
-                        for(size_t b=0; b<priority.size();b++){
-                            if (n==priority[b])flag = true;
-                        }
-
-                    }
-                }
-                if (flag==true){
-                    priority.push_back(n);
-                    cout<<n <<"  enter  \n";
-                    unsorted--;
-                }
-
-
-            }
-            
-            
-        }
-        else {
-            return priority;
-            break;
-        }
-    }*/
-
-
-    /*for (size_t n = 0;n<p.size();n++){
-        flag = true;
-        for (size_t c = 0 ;c<p.size();c++){
-            if (p[c]>p[n]){
-                flag = false;
-                for(size_t m; m<priority.size();m++)
-
-            }
-        }
-
-        
-    }*/
 }
+
 
 int main(){
     string line ;
@@ -150,6 +74,9 @@ int main(){
     int numtask;
     int temp_extime;
     int temp_period;
+    int T_step = 0;
+    int lcm;
+
 
    
     
@@ -163,6 +90,8 @@ int main(){
     vector <int> extime;
     vector <int>period;
     vector <int> prior;
+    vector <int> current_execution;
+    int active_task;
 
     int temp=0;
 
@@ -175,12 +104,69 @@ int main(){
         extime.push_back(temp_extime);
         period.push_back(temp_period);
     }
-    lowest_common_multiple(period);
+    lcm = lowest_common_multiple(period);
     prior = find_priority(period);
     for (size_t n; n<prior.size();n++){
         cout<< prior[n]<< " ";
     }
     cout<<"\n";
+
+    cout<< "---------------------------------------------------------------------------\n";
+    current_execution = extime;
+    bool no_task=false;
+
+    while (T_step < lcm){
+
+
+        //get active task
+        for(size_t n = prior.size(); n>0; n--){
+            if (current_execution[prior[n-1]]>0){
+                active_task = prior[n-1];
+            }    
+
+
+        }
+        no_task=true;
+        for( auto m:current_execution){
+            if (m>0){
+                no_task=false;
+            }
+
+        }
+
+        if (!no_task){
+
+
+            cout<<T_step<<" Task " << active_task+1 << " executes \n";
+
+
+
+       
+            current_execution[active_task]--;
+            if(current_execution[active_task]==0){
+                cout<<T_step<<" Task " << active_task+1 << " completed \n";
+            }
+            
+        }
+        else if(no_task)cout<<T_step<<" No task \n";
+        T_step++;
+         //adding the execution time every period reached
+        for(size_t n = 0; n<period.size();n++){
+            if(T_step % period[n]==0){
+                if(current_execution[n]!=0){
+                    cout<<T_step<<" Task " << n+1 << " misses \n";
+                }
+                current_execution[n] = extime[n];
+
+            }
+        }
+
+
+        
+
+
+    }
+    cout<< "---------------------------------------------------------------------------\n";
     return 0;
 
 
